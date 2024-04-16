e2616d57e81c88b01503729cbacfcc5d648e06cd
Rajesh <rajesh.doari50@gmail.com>
2024-04-16 17:37:45 +0530

    change in state

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import ProductDate from "./ProductDate";
 import Card from "./Card";
 
-const ProductItem = ({ ...props }) => {
+const ProductItem = ({ title, amount, date }) => {
   console.log("card rendered");
 
-  const [title, setTitle] = useState(props.title);
+  const [brand, setBrand] = useState(title);
   const changeHandler = () => {};
   return (
     <Card className="product-item">
-      <ProductDate date={props.date} />
+      <ProductDate date={date} />
       <div className="product-title">
-        <h2>{title}</h2>
+        <h2>{brand}</h2>
+      </div>
+      <div className="product-amount">
+        <span>{amount}</span>
       </div>
       <button onClick={changeHandler}>change title</button>
     </Card>
